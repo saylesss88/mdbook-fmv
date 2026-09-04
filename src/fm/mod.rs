8bcfc274803764643aa9c85b57b16fa16c3dc221
saylesss88 <saylesss87@proton.me>
2026-09-04 08:51:26 -0400

    test: add failing test missing_date_produces_diagnostic

--- src/fm/mod.rs
+++ src/fm/mod.rs
@@ -26,4 +26,12 @@ mod tests {
         assert_eq!(diags.len(), 1);
         assert_eq!(diags[0].code, "fm::missing-frontmatter");
     }
+
+    #[test]
+    fn missing_date_produces_diagnostic() {
+        let content = "---\ntitle: Hello\nauthor: Tom\n---\n\nSome content.\n";
+        let diags = check_frontmatter(content);
+        assert_eq!(diags.len(), 1);
+        assert_eq!(diags[0].code, "fm::missing-date");
+    }
 }
